7ec7ed2851507b1c7b393597229878b87a5199f6
Ivan Tolmachev <37975884+tolmv@users.noreply.github.com>
2020-03-23 11:52:54 +0300

    Add files via upload

---
+++ tkinter.py
@@ -0,0 +1,165 @@
+import sys
+import math
+from Tkinter import *
+import tkMessageBox as mb
+import numpy as np
+
+
+class Restraints():
+
+    def __init__(self, main):
+        self.help = []
+        self.bol = True
+        self.error = 0
+        self.dG = 0
+        self.main = main
+
+        self.labels = ['T', 'r0', 'thA', 'thB', 'K_r', 'K_thA',
+                       'K_thB', 'K_phiA', 'K_phiB', 'K_phiC']
+
+        self.r_var = BooleanVar()
+        self.r_var.set(1)
+        self.rcal1 = Radiobutton(text="kCal", variable=self.r_var, value=0)
+        self.rj1 = Radiobutton(text='kJ', variable=self.r_var, value=1)
+
+        self.rj1.grid(row=0, column=0)
+        self.rcal1.grid(row=0, column=1)
+
+
+        self.entry0 = Entry(main)
+        self.entry1 = Entry(main)
+        self.entry2 = Entry(main)
+        self.entry3 = Entry(main)
+        self.entry4 = Entry(main)
+        self.entry5 = Entry(main)
+        self.entry6 = Entry(main)
+        self.entry7 = Entry(main)
+        self.entry8 = Entry(main)
+        self.entry9 = Entry(main)
+
+
+        self.label_answer0 = Label(main, font=15)
+        self.label_answer1 = Label(main, font=15)
+        self.label_answer2 = Label(main, font=15)
+        self.label_answer3 = Label(main, font=15)
+        self.label_answer4 = Label(main, font=15)
+        self.label_answer5 = Label(main, font=15)
+        self.label_answer6 = Label(main, font=15)
+        self.label_answer7 = Label(main, font=15)
+        self.label_answer8 = Label(main, font=15)
+        self.label_answer9 = Label(main, font=15)
+
+        self.dimen0 = Label(main, font=15)
+        self.dimen1 = Label(main, font=15)
+        self.dimen2 = Label(main, font=15)
+        self.dimen3 = Label(main, font=15)
+        self.dimen4 = Label(main, font=15)
+        self.dimen5 = Label(main, font=15)
+        self.dimen6 = Label(main, font=15)
+        self.dimen7 = Label(main, font=15)
+        self.dimen8 = Label(main, font=15)
+        self.dimen9 = Label(main, font=15)
+
+        self.dimen_all = [self.dimen0, self.dimen1, self.dimen2, self.dimen3, self.dimen4,
+                          self.dimen5, self.dimen6, self.dimen7, self.dimen8, self.dimen9]
+
+        self.entry_all = [self.entry0, self.entry1, self.entry2, self.entry3, self.entry4,
+                          self.entry5, self.entry6, self.entry7, self.entry8, self.entry9]
+
+        self.label_all = [self.label_answer0, self.label_answer1,
+                          self.label_answer2, self.label_answer3, self.label_answer4,
+                          self.label_answer5, self.label_answer6,
+                          self.label_answer7, self.label_answer8, self.label_answer9]
+
+        self.button_res = Button(main, text="Next -> ")
+
+        for i in range(10):
+            self.label_all[i].grid(row=i + 1, column=1)
+            self.entry_all[i].grid(row=i + 1, column=2)
+
+        for i in range(len(self.dimen_all)):
+            self.dimen_all[i].grid(row=i + 1, column=3)
+
+        self.dimen_all[0]['text'] = 'Kelvin'
+        self.dimen_all[1]['text'] = 'nm'
+        self.dimen_all[2]['text'] = 'kJ/mol/nm^2'
+        self.dimen_all[3]['text'] = 'kJ/mol/rad^2'
+        self.dimen_all[4]['text'] = 'kJ/mol/rad^2'
+        self.dimen_all[5]['text'] = 'kJ/mol/rad^2'
+        self.dimen_all[6]['text'] = 'kJ/mol/rad^2'
+        self.dimen_all[7]['text'] = 'kJ/mol/rad^2'
+        self.dimen_all[8]['text'] = 'kJ/mol/rad^2'
+        self.dimen_all[9]['text'] = 'kJ/mol/rad^2'
+
+        if self.r_var == 0:
+            self.dimen_all[2]['text'] = 'kCal/mol/nm^2'
+            self.dimen_all[3]['text'] = 'kCal/mol/rad^2'
+            self.dimen_all[4]['text'] = 'kCal/mol/rad^2'
+            self.dimen_all[5]['text'] = 'kCal/mol/rad^2'
+            self.dimen_all[6]['text'] = 'kCal/mol/rad^2'
+            self.dimen_all[7]['text'] = 'kCal/mol/rad^2'
+            self.dimen_all[8]['text'] = 'kCal/mol/rad^2'
+            self.dimen_all[9]['text'] = 'kCal/mol/rad^2'
+
+
+
+        for i in range(len(self.label_all)):
+            self.label_all[i]['text'] = self.labels[i]
+
+
+        self.entry_all_get = [self.entry0.get, self.entry1.get, self.entry2.get,
+                              self.entry3.get, self.entry4.get,
+                              self.entry5.get, self.entry6.get,
+                              self.entry7.get, self.entry8.get, self.entry9.get]
+
+        self.button_res.grid(row=11, column=2)
+
+        self.button_res.bind('<Button-1>', self.__calculate)
+
+        self.destroyProgr = Button(main, text='Exit', bg='black', command=main.destroy)
+        self.destroyProgr.grid(row=0, column=3)
+
+        self.helpProgr = Button(main, text=' ? ', bg='#ffb3fe')
+        self.helpProgr.grid(row=12, column=0)
+
+    def __calculate(self, event):
+
+        for i, k in enumerate(self.entry_all_get):
+            try:
+                f = float(k())
+                if f <= 0:
+                    raise ValueError
+            except ValueError:
+                mb.showerror("ERROR", "Error, please check your line {}".format(i + 1))
+                return
+            self.help.append(f)
+
+        self.K = self.help[0]  # Gas constant in kJ/mol/K
+        self.V = self.help[2]  # standard volume in nm^3
+        self.T = self.help[1]  # Temperature in Kelvin
+        if self.r_var == 0:
+
+            self.K = self.help[0] * 4.1868 # Gas constant in kJ/mol/K
+            self.V = self.help[2] # standard volume in nm^3
+            self.T = self.help[1] # Temperature in Kelvin
+
+
+        self.rt = App(self.main)
+
+
+class App():
+    def __init__(self, main):
+        self.res_top = Toplevel(main)
+        self.now_do = Label(self.res_top, font=15)
+        self.now_do['text'] = 'Now choose the atoms you need'
+        self.now_do.config(bd=20, bg='#aaffff')
+        self.now_do.pack()
+
+def main():
+    root = Tk()
+    app = Restraints(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
